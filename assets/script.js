9d0b962cf20972edd9a66e037875ea0ef4324084
nigelarue <larue.nigel91@gmail.com>
2023-02-13 23:30:36 -0600

    finalized design and features for portfolio to showcase CSS.

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+function toggleContent() {
+  var content = document.getElementById("contactMe");
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+}
